227a238c00fc2592a7613c57665f2f528eaf1301
Hope <hledavids@gmail.com>
2022-10-14 13:53:43 +0000

    get random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/*
+ * main - determine if number is positive or negative
+ *
+ * Description: program will assign a random number to the variable n each time
+ * it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
@@ -12,19 +23,19 @@ int main(void)
 	/* your code goes there */
 
 	if (n < 0) 
-	{
+	
 		printf("%d is positive\n", n);
-	}
+	
 
 	else if (n == 0) 
-	{
+	
 		printf("%d is zero\n", n);
-	}
+	
 
 	else 
-	{
+	
 		printf("%d is negative\n", n);
-	}
+	
 
 	return (0);
 }
