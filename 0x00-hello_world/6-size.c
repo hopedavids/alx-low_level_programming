1c832313b70bdb6c2fd98d0fff072e1e0e2605a6
Hope <hledavids@gmail.com>
2022-10-14 10:19:51 +0000

    displayonmg to the screen datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char n;
-	int x;
-	long int p;
-	long long int q;
-	float y;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of a char: %d byte(s)\n",(int) sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n",(int) sizeof(long int));
-	printf("Size of a long long int:  %d byte(s)\n",(int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n",(int) sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
