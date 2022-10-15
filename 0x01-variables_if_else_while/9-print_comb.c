a967fb0a89c60d9b0ff1e716e78f05e2342a1551
Hope <hledavids@gmail.com>
2022-10-15 14:19:15 +0000

    using puchar to print all possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all combinations of single digits using putchar
+ *
+ * Return: Always 0 for Success
+ */
+
+int main(void){
+	
+	int comb;
+
+	for(comb = '0'; comb <= '9'; comb++){
+		
+		putchar(comb);
+		
+		if(comb != '9'){
+		
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
