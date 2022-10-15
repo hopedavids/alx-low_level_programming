43f64334fdf06bb553428ee9008d06bb65aa96aa
Hope <hledavids@gmail.com>
2022-10-15 14:01:46 +0000

    printing base16 using puchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: printing out base16 in lowercase
+ *
+ * Return: Always 0 for success.
+ */
+int main(void){
+	
+	char ch;
+	int b16;
+
+	for(b16 = '0'; b16 <= '9'; b16++){
+		putchar(b16);
+	}
+	for(ch = 'a'; ch <= 'f'; ch++){
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
