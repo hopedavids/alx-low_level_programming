8d896199219f7a10c14a4f906b052e775c5f381c
Hope <hledavids@gmail.com>
2022-10-15 13:49:54 +0000

    reversed strings using putchar and forloop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * main - Entry point
+ *
+ * Description : print the alphabeth in reverse using putchar
+ *
+ * Return: Always 0 for Success
+ */
+
+int main(void){
+    
+	char ch;
+	
+	for(ch = 'z'; ch >= 'a'; ch--){
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
