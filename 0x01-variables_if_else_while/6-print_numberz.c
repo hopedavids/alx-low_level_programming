fafeee034d6f4970c2ab2a66b05bcc8d93465e22
Hope <hledavids@gmail.com>
2022-10-15 13:33:24 +0000

    using puchar with integers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using putchar to handle integer values
+ *
+ * Return: return 0 for Success
+ */
+int main(void){
+		
+int b10;
+
+for(b10 = '0'; b10 <= '9'; b10++){
+
+	putchar(b10);
+}
+putchar('\n');
+return (0);
+}
