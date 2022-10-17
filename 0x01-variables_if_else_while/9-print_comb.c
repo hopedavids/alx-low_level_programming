7d8ca25c3bdc3afb3390bb17e83ea58f0ff2f661
Hope <hledavids@gmail.com>
2022-10-17 21:58:18 +0000

    betty smile on 9-print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,19 @@
  * Return: Always 0 for Success
  */
 
-int main(void){
-	
+int main(void)
+{
 	int comb;
 
-	for(comb = '0'; comb <= '9'; comb++){
-		
+	for (comb = '0'; comb <= '9'; comb++)
+	{
 		putchar(comb);
-		
-		if(comb != '9'){
-		
+
+		if (comb != '9')
+		{
 			putchar(',');
 			putchar(' ');
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
