0dcdd4045a5d4d5dc67238aa89799636b8f5c127
Hope <hledavids@gmail.com>
2022-10-17 21:50:28 +0000

    betty smiles on 8-print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Description: printing out base16 in lowercase
  *
  * Return: Always 0 for success.
  */
-int main(void){
-	
+int main(void)
+{
 	char ch;
 	int b16;
 
-	for(b16 = '0'; b16 <= '9'; b16++){
+	for (b16 = '0'; b16 <= '9'; b16++)
+	{
 		putchar(b16);
 	}
-	for(ch = 'a'; ch <= 'f'; ch++){
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
 		putchar(ch);
 	}
 	putchar('\n');
