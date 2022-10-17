d7ee7364975da65a6cd49553d34d4d0bcf2aedb5
Hope <hledavids@gmail.com>
2022-10-17 21:05:48 +0000

    betty 3-print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,14 +13,13 @@
 int main(void)
 {
 	char ch;
-	
-	for(ch = 'a'; ch <= 'z'; ch++)
-	
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
- 	
-	for(ch = 'A'; ch <= 'Z'; ch++)
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
 
 	{
 		putchar(ch);
