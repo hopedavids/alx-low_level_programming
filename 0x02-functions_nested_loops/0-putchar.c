203196f7fa63333133c3e240ecf334ae19f4928a
Hope <hledavids@gmail.com>
2022-10-18 20:10:56 +0000

    building your own func

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "_putchar.h"
+
+/**
+ * main - Entry point
+ * Description: This program prints out the _putchar
+ * Return: Program must have a return value
+ *
+ */
+
+int main(void)
+{
+	char i = '2';
+
+	_putchar(i);
+	_putchar('\n');
+
+	return (0);
+}
