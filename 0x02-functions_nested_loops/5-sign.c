b17958aa21c71cce936d13f53743ec86b2c6dc65
Hope <hledavids@gmail.com>
2022-10-19 07:55:31 +0000

    works for @n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n : variable n
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
