9dc4ed156fd51c57774db165e4ac42e5309e0e33
Hope <hledavids@gmail.com>
2022-10-19 10:10:57 +0000

    corrected 8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H <= 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+
+			_putchar(H / 10 + '0');
+			_putchar(H % 10 + '0');
+			_putchar(':');
+			_putchar(M / 10 + '0');
+			_putchar(M % 10 + '0');
+			_putchar('\n');
+		}
+	}
+
+}
