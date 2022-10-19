2cda65e77cd2ddc3812d82cd866fad2e110cc5c7
Hope <hledavids@gmail.com>
2022-10-19 07:26:59 +0000

    changes saved

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
- * int _isalpha(int c) - the function name
- * Description - 
- * will return an integer
+ * _isalpha - the function name
+ * @c: describes the code parameter
+ * Return: will return an integer
  */
 int _isalpha(int c)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-                for (i = 'A'; i <= 'Z'; i++)
-
-                        if (i != c)
-                        {
-                                return 1;
-                        }
-			else
-				return (0);
-        }
-	return 0;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			return (1);
+		else
+			return (0);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
 }
 
