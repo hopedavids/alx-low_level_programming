5e1952c95ecb76f420338672c1e0121705dbbb5e
Hope <hledavids@gmail.com>
2022-10-25 09:49:56 +0000

    the first pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function name
+ * @n: the name of the parameter variable
+ * Return: No return value because it's of type void
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
