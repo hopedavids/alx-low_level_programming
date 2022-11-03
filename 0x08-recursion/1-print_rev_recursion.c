a9b20f6a684ef0517873b0138bd5ba1d5fddcca7
Hope <hledavids@gmail.com>
2022-11-03 20:54:41 +0000

    1-recursive

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - the function name, check code
+ * @s: pointer to a variable whose datatype is a string
+ * Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+		
+	}
+
+}
