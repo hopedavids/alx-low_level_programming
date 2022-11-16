1865b2596eca880e144d68f75f4986226a9402d0
Hope <hledavids@gmail.com>
2022-11-16 16:45:48 +0000

    just push

---
+++ 0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - the name of the function
+ * @dest: the parameter parsed into the function
+ * @src: the src parameter parsed into the function
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	
+
+}
